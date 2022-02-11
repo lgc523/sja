c255d98db44e6e97991ab49b8548fdc4410e437b
spider <lgc523@126.com>
2022-02-11 22:49:50 +0800

    feat: merge

--- src/main/java/dev/spider/Main.java
+++ src/main/java/dev/spider/Main.java
@@ -1,8 +1,13 @@
 package dev.spider;
 
+import java.util.UUID;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        UUID uuid = UUID.randomUUID();
+        String s = uuid.toString();
+        System.out.println(s);
     }
 }
